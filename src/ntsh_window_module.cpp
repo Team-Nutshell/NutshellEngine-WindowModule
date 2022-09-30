ddc6dd38e028b3955e4978b4daf231c0c05a3bc1
ZaOniRinku <zaonirinku@gmail.com>
2022-10-01 00:05:42 +0200

    replace error for warning for non-implemented functions

--- src/ntsh_window_module.cpp
+++ src/ntsh_window_module.cpp
@@ -4,16 +4,16 @@
 #include "../external/Common/ntsh_engine_enums.h"
 
 void NutshellWindowModule::init() {
-    NTSH_MODULE_ERROR("init() function not implemented.", NTSH_RESULT_MODULE_FUNCTION_NOT_IMPLEMENTED);
+    NTSH_MODULE_WARNING("init() function not implemented.");
 }
 
 void NutshellWindowModule::update(double dt) {
     NTSH_UNUSED(dt);
-    NTSH_MODULE_ERROR("update() function not implemented.", NTSH_RESULT_MODULE_FUNCTION_NOT_IMPLEMENTED);
+    NTSH_MODULE_WARNING("update() function not implemented.");
 }
 
 void NutshellWindowModule::destroy() {
-    NTSH_MODULE_ERROR("destroy() function not implemented.", NTSH_RESULT_MODULE_FUNCTION_NOT_IMPLEMENTED);
+    NTSH_MODULE_WARNING("destroy() function not implemented.");
 }
 
 extern "C" NTSH_MODULE_API NutshellWindowModuleInterface * createModule() {
