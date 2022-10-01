5eaf34d580598baffa81275218397db3a710f8fb
ZaOniRinku <zaonirinku@gmail.com>
2022-10-01 14:08:09 +0200

    add new functions

--- src/ntsh_window_module.h
+++ src/ntsh_window_module.h
@@ -8,4 +8,21 @@ public:
     void init();
     void update(double dt);
     void destroy();
+
+    // Send a request to close the window
+    virtual void closeWindow();
+    // Returns true if there has been a request to close the window, else if there has been no request to close the window
+    virtual bool shouldClose();
+
+    // Changes the size of the window to width and height
+    void setWindowSize(int width, int height);
+    // Returns the width of the window
+    int getWindowWidth();
+    // Returns the height of the window
+    int getWindowHeight();
+
+    // Returns true if the window is in fullscreen mode, else, return false
+    bool isFullscreen();
+    // If the fullscreen parameter is true, puts the window in fullscreen, else, puts the window in windowed
+    void setFullscreen(bool fullscreen);
 };
