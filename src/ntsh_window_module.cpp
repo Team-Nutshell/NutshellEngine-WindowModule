2ab3b6929e024a95bb35001bf6f126a4c60d94e3
ZaOniRinku <zaonirinku@gmail.com>
2022-10-01 16:11:29 +0200

    replaces spaces for tabs

--- src/ntsh_window_module.cpp
+++ src/ntsh_window_module.cpp
@@ -4,57 +4,57 @@
 #include "../external/Common/ntsh_engine_enums.h"
 
 void NutshellWindowModule::init() {
-    NTSH_MODULE_WARNING("init() function not implemented.");
+	NTSH_MODULE_WARNING("init() function not implemented.");
 }
 
 void NutshellWindowModule::update(double dt) {
-    NTSH_UNUSED(dt);
-    NTSH_MODULE_WARNING("update() function not implemented.");
+	NTSH_UNUSED(dt);
+	NTSH_MODULE_WARNING("update() function not implemented.");
 }
 
 void NutshellWindowModule::destroy() {
-    NTSH_MODULE_WARNING("destroy() function not implemented.");
+	NTSH_MODULE_WARNING("destroy() function not implemented.");
 }
 
 void NutshellWindowModule::closeWindow() {
-    NTSH_MODULE_WARNING("closeWindow() function not implemented.");
+	NTSH_MODULE_WARNING("closeWindow() function not implemented.");
 }
 
 bool NutshellWindowModule::shouldClose() {
-    NTSH_MODULE_WARNING("shouldClose() function not implemented.");
-    return false;
+	NTSH_MODULE_WARNING("shouldClose() function not implemented.");
+	return false;
 }
 
 void NutshellWindowModule::setWindowSize(int width, int height) {
-    NTSH_UNUSED(width);
-    NTSH_UNUSED(height);
-    NTSH_MODULE_WARNING("setWindowSize() function not implemented.");
+	NTSH_UNUSED(width);
+	NTSH_UNUSED(height);
+	NTSH_MODULE_WARNING("setWindowSize() function not implemented.");
 }
 
 int NutshellWindowModule::getWindowWidth() {
-    NTSH_MODULE_WARNING("getWindowWidth() function not implemented.");
-    return 0;
+	NTSH_MODULE_WARNING("getWindowWidth() function not implemented.");
+	return 0;
 }
 
 int NutshellWindowModule::getWindowHeight() {
-    NTSH_MODULE_WARNING("getWindowHeight() function not implemented.");
-    return 0;
+	NTSH_MODULE_WARNING("getWindowHeight() function not implemented.");
+	return 0;
 }
 
 bool NutshellWindowModule::isFullscreen() {
-    NTSH_MODULE_WARNING("isFullscreen() function not implemented.");
-    return false;
+	NTSH_MODULE_WARNING("isFullscreen() function not implemented.");
+	return false;
 }
 
 void NutshellWindowModule::setFullscreen(bool fullscreen) {
-    NTSH_UNUSED(fullscreen);
-    NTSH_MODULE_WARNING("setFullscreen() function not implemented.");
+	NTSH_UNUSED(fullscreen);
+	NTSH_MODULE_WARNING("setFullscreen() function not implemented.");
 }
 
 extern "C" NTSH_MODULE_API NutshellWindowModuleInterface * createModule() {
-    return new NutshellWindowModule;
+	return new NutshellWindowModule;
 }
 
 extern "C" NTSH_MODULE_API void destroyModule(NutshellWindowModuleInterface * m) {
-    delete m;
+	delete m;
 }
