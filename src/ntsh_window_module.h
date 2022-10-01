59b9ed5b54400ccb7873f3f995d5aeb0237c4cc3
ntsh-oni <teamnutshell.dev@gmail.com>
2022-10-01 14:48:12 +0200

    implementation of the new functions

--- src/ntsh_window_module.h
+++ src/ntsh_window_module.h
@@ -5,6 +5,19 @@
 class NutshellWindowModule : public NutshellWindowModuleInterface {
 private:
     GLFWwindow* m_window = nullptr;
+    int m_width = 1280;
+    int m_height = 720;
+    int m_x = 0;
+    int m_y = 0;
+
+    // Resize callback
+    static void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+        NTSH_UNUSED(width);
+        NTSH_UNUSED(height);
+        auto ptr = reinterpret_cast<NutshellWindowModule*>(glfwGetWindowUserPointer(window));
+        ptr->getWindowWidth();
+        ptr->getWindowHeight();
+    }
 public:
     NutshellWindowModule() : NutshellWindowModuleInterface("Nutshell Window Test Module") {}
 
