e750a1fd2d026d2656f46ef1764913eb88b8cb4b
ntsh-oni <teamnutshell.dev@gmail.com>
2022-10-01 16:13:53 +0200

    replaces spaces for tabs

--- src/ntsh_window_module.cpp
+++ src/ntsh_window_module.cpp
@@ -4,76 +4,76 @@
 #include "../external/Common/ntsh_engine_enums.h"
 
 void NutshellWindowModule::init() {
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    m_window = glfwCreateWindow(m_width, m_height, m_name.c_str(), nullptr, nullptr);
-    glfwGetWindowPos(m_window, &m_x, &m_y);
-    glfwSetWindowUserPointer(m_window, this);
-    
-    glfwSetFramebufferSizeCallback(m_window, framebufferSizeCallback);
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	m_window = glfwCreateWindow(m_width, m_height, m_name.c_str(), nullptr, nullptr);
+	glfwGetWindowPos(m_window, &m_x, &m_y);
+	glfwSetWindowUserPointer(m_window, this);
+	
+	glfwSetFramebufferSizeCallback(m_window, framebufferSizeCallback);
 }
 
 void NutshellWindowModule::update(double dt) {
-    NTSH_UNUSED(dt);
-    glfwPollEvents();
+	NTSH_UNUSED(dt);
+	glfwPollEvents();
 }
 
 void NutshellWindowModule::destroy() {
-    glfwDestroyWindow(m_window);
-    glfwTerminate();
+	glfwDestroyWindow(m_window);
+	glfwTerminate();
 }
 
 void NutshellWindowModule::closeWindow() {
-    glfwSetWindowShouldClose(m_window, true);
+	glfwSetWindowShouldClose(m_window, true);
 }
 
 bool NutshellWindowModule::shouldClose() {
-    return glfwWindowShouldClose(m_window);
+	return glfwWindowShouldClose(m_window);
 }
 
 void NutshellWindowModule::setWindowSize(int width, int height) {
-    glfwSetWindowSize(m_window, width, height);
-    m_width = width;
-    m_height = height;
+	glfwSetWindowSize(m_window, width, height);
+	m_width = width;
+	m_height = height;
 }
 
 int NutshellWindowModule::getWindowWidth() {
-    int width;
-    int height;
-    glfwGetWindowSize(m_window, &width, &height);
-    return width;
+	int width;
+	int height;
+	glfwGetWindowSize(m_window, &width, &height);
+	return width;
 }
 
 int NutshellWindowModule::getWindowHeight() {
-    int width;
-    int height;
-    glfwGetWindowSize(m_window, &width, &height);
-    return height;
+	int width;
+	int height;
+	glfwGetWindowSize(m_window, &width, &height);
+	return height;
 }
 
 bool NutshellWindowModule::isFullscreen() {
-    return glfwGetWindowMonitor(m_window) != nullptr;
+	return glfwGetWindowMonitor(m_window) != nullptr;
 }
 
 void NutshellWindowModule::setFullscreen(bool fullscreen) {
-    if (!isFullscreen() && fullscreen) {
-        glfwGetWindowPos(m_window, &m_x, &m_y);
-        glfwGetWindowSize(m_window, &m_width, &m_height);
+	if (!isFullscreen() && fullscreen) {
+		glfwGetWindowPos(m_window, &m_x, &m_y);
+		glfwGetWindowSize(m_window, &m_width, &m_height);
 
-        GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
-        const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
-        glfwSetWindowMonitor(m_window, primaryMonitor, 0, 0, mode->width, mode->height, GLFW_DONT_CARE);
-    }
-    else if (!fullscreen && isFullscreen()) {
-        GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
-        glfwSetWindowMonitor(m_window, primaryMonitor, m_x, m_y, m_width, m_height, GLFW_DONT_CARE);
-    }
+		GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+		const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
+		glfwSetWindowMonitor(m_window, primaryMonitor, 0, 0, mode->width, mode->height, GLFW_DONT_CARE);
+	}
+	else if (!fullscreen && isFullscreen()) {
+		GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+		glfwSetWindowMonitor(m_window, primaryMonitor, m_x, m_y, m_width, m_height, GLFW_DONT_CARE);
+	}
 }
 
 extern "C" NTSH_MODULE_API NutshellWindowModuleInterface* createModule() {
-    return new NutshellWindowModule;
+	return new NutshellWindowModule;
 }
 
 extern "C" NTSH_MODULE_API void destroyModule(NutshellWindowModuleInterface* m) {
-    delete m;
+	delete m;
 }
--- src/ntsh_window_module.h
+++ src/ntsh_window_module.h
@@ -4,41 +4,41 @@
 
 class NutshellWindowModule : public NutshellWindowModuleInterface {
 private:
-    GLFWwindow* m_window = nullptr;
-    int m_width = 1280;
-    int m_height = 720;
-    int m_x = 0;
-    int m_y = 0;
+	GLFWwindow* m_window = nullptr;
+	int m_width = 1280;
+	int m_height = 720;
+	int m_x = 0;
+	int m_y = 0;
 
-    // Resize callback
-    static void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
-        NTSH_UNUSED(width);
-        NTSH_UNUSED(height);
-        auto ptr = reinterpret_cast<NutshellWindowModule*>(glfwGetWindowUserPointer(window));
-        ptr->getWindowWidth();
-        ptr->getWindowHeight();
-    }
+	// Resize callback
+	static void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+		NTSH_UNUSED(width);
+		NTSH_UNUSED(height);
+		auto ptr = reinterpret_cast<NutshellWindowModule*>(glfwGetWindowUserPointer(window));
+		ptr->getWindowWidth();
+		ptr->getWindowHeight();
+	}
 public:
-    NutshellWindowModule() : NutshellWindowModuleInterface("Nutshell Window Test Module") {}
+	NutshellWindowModule() : NutshellWindowModuleInterface("Nutshell Window Test Module") {}
 
-    void init();
-    void update(double dt);
-    void destroy();
+	void init();
+	void update(double dt);
+	void destroy();
 
-    // Send a request to close the window
-    virtual void closeWindow();
-    // Returns true if there has been a request to close the window, else if there has been no request to close the window
-    virtual bool shouldClose();
+	// Send a request to close the window
+	virtual void closeWindow();
+	// Returns true if there has been a request to close the window, else if there has been no request to close the window
+	virtual bool shouldClose();
 
-    // Changes the size of the window to width and height
-    void setWindowSize(int width, int height);
-    // Returns the width of the window
-    int getWindowWidth();
-    // Returns the height of the window
-    int getWindowHeight();
+	// Changes the size of the window to width and height
+	void setWindowSize(int width, int height);
+	// Returns the width of the window
+	int getWindowWidth();
+	// Returns the height of the window
+	int getWindowHeight();
 
-    // Returns true if the window is in fullscreen mode, else, return false
-    bool isFullscreen();
-    // If the fullscreen parameter is true, puts the window in fullscreen, else, puts the window in windowed
-    void setFullscreen(bool fullscreen);
+	// Returns true if the window is in fullscreen mode, else, return false
+	bool isFullscreen();
+	// If the fullscreen parameter is true, puts the window in fullscreen, else, puts the window in windowed
+	void setFullscreen(bool fullscreen);
 };
