0dc73c99cee1f5fa67400b4cfcb014a675bf3b72
ZaOniRinku <zaonirinku@gmail.com>
2022-10-16 19:40:44 +0200

    replace functions non implemented warnings

--- src/ntsh_window_module.cpp
+++ src/ntsh_window_module.cpp
@@ -5,70 +5,70 @@
 #include "../external/Common/ntsh_engine_enums.h"
 
 void NutshellWindowModule::init() {
-	NTSH_MODULE_WARNING("init() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 void NutshellWindowModule::update(double dt) {
 	NTSH_UNUSED(dt);
-	NTSH_MODULE_WARNING("update() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 void NutshellWindowModule::destroy() {
-	NTSH_MODULE_WARNING("destroy() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 void NutshellWindowModule::close() {
-	NTSH_MODULE_WARNING("closeWindow() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 bool NutshellWindowModule::shouldClose() {
-	NTSH_MODULE_WARNING("shouldClose() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 	return false;
 }
 
 void NutshellWindowModule::setSize(int width, int height) {
 	NTSH_UNUSED(width);
 	NTSH_UNUSED(height);
-	NTSH_MODULE_WARNING("setWindowSize() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 int NutshellWindowModule::getWidth() {
-	NTSH_MODULE_WARNING("getWindowWidth() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 	return 0;
 }
 
 int NutshellWindowModule::getHeight() {
-	NTSH_MODULE_WARNING("getWindowHeight() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 	return 0;
 }
 
 bool NutshellWindowModule::isFullscreen() {
-	NTSH_MODULE_WARNING("isFullscreen() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 	return false;
 }
 
 void NutshellWindowModule::setFullscreen(bool fullscreen) {
 	NTSH_UNUSED(fullscreen);
-	NTSH_MODULE_WARNING("setFullscreen() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 void NutshellWindowModule::pollEvents() {
-	NTSH_MODULE_WARNING("pollEvents() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 void NutshellWindowModule::setTitle(const std::string& title) {
 	NTSH_UNUSED(title);
-	NTSH_MODULE_WARNING("setTitle() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 }
 
 #ifdef NTSH_OS_WINDOWS
 HWND NutshellWindowModule::getNativeHandle() {
-	NTSH_MODULE_WARNING("getNativeHandle() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 	return nullptr;
 }
 #elif NTSH_OS_LINUX
 Window NutshellWindowModule::getNativeHandle() {
-	NTSH_MODULE_WARNING("getNativeHandle() function not implemented.");
+	NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED();
 	return 0;
 }
 #endif
