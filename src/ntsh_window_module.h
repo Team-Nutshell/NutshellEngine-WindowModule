2e006aa61b5d81fed3b29e702248da9779e35b0f
ntsh-oni <teamnutshell.dev@gmail.com>
2022-12-13 17:54:55 +0100

    implement the cursor visibility functions

--- src/ntsh_window_module.h
+++ src/ntsh_window_module.h
@@ -41,12 +41,17 @@ public:
 	// Gets the state of the mouse button. None is the input is neutral, Pressed the first frame it is being pressed, Held from the second frame it is pressed, Released the frame it is being released
 	NtshInputState getButtonState(NtshInputMouseButton button);
 
-	// Sets the mouse position
-	void setMousePosition(int x, int y);
-	// Gets the mouse horizontal position
-	int getMouseXPosition();
-	// Gets the mouse vertical position
-	int getMouseYPosition();
+	// Sets the mouse cursor position
+	void setCursorPosition(int x, int y);
+	// Gets the mouse cursor horizontal position
+	int getCursorXPosition();
+	// Gets the mouse cursor vertical position
+	int getCursorYPosition();
+
+	// Returns true if the mouse cursor is visible, else, returns false
+	bool isCursorVisible();
+	// If the mouse cursor is visible, hides it, else, shows it
+	void setCursorVisibility(bool visible);
 
 #ifdef NTSH_OS_WINDOWS
 	// Returns the native Win32 window handle
@@ -60,4 +65,6 @@ private:
 	std::unique_ptr<QApplication> m_application;
 
 	std::unique_ptr<QtWindow> m_window;
+
+	bool m_cursorVisible = true;
 };
