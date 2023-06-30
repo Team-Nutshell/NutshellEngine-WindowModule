4cb152dba0b45257e533196a7147912d2cdad85c
ntsh-oni <teamnutshell.dev@gmail.com>
2023-06-30 16:57:03 +0200

    minor fixes

--- src/ntshengn_window_module.cpp
+++ src/ntshengn_window_module.cpp
@@ -42,13 +42,13 @@ NtshEngn::WindowID NtshEngn::WindowModule::open(int width, int height, const std
 	return m_id++;
 }
 
-bool NtshEngn::WindowModule::isOpen(WindowID windowId) {
-	return (m_windows.find(windowId) != m_windows.end()) ? !m_windows[windowId]->shouldClose() : false;
+bool NtshEngn::WindowModule::isOpen(WindowID windowID) {
+	return (m_windows.find(windowID) != m_windows.end()) ? !m_windows[windowID]->shouldClose() : false;
 }
 
-void NtshEngn::WindowModule::close(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	m_windows[windowId]->close();
+void NtshEngn::WindowModule::close(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	m_windows[windowID]->close();
 }
 
 NtshEngn::WindowID NtshEngn::WindowModule::getMainWindowID() {
@@ -66,118 +66,118 @@ uint64_t NtshEngn::WindowModule::windowCount() {
 	return count;
 }
 
-void NtshEngn::WindowModule::setSize(WindowID windowId, int width, int height) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	m_windows[windowId]->setSize(width, height);
+void NtshEngn::WindowModule::setSize(WindowID windowID, int width, int height) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	m_windows[windowID]->setSize(width, height);
 }
 
-int NtshEngn::WindowModule::getWidth(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->getWidth();
+int NtshEngn::WindowModule::getWidth(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->getWidth();
 }
 
-int NtshEngn::WindowModule::getHeight(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->getHeight();
+int NtshEngn::WindowModule::getHeight(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->getHeight();
 }
 
-void NtshEngn::WindowModule::setPosition(WindowID windowId, int x, int y) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	m_windows[windowId]->setPosition(x, y);
+void NtshEngn::WindowModule::setPosition(WindowID windowID, int x, int y) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	m_windows[windowID]->setPosition(x, y);
 }
 
-int NtshEngn::WindowModule::getPositionX(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->getPositionX();
+int NtshEngn::WindowModule::getPositionX(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->getPositionX();
 }
 
-int NtshEngn::WindowModule::getPositionY(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->getPositionY();
+int NtshEngn::WindowModule::getPositionY(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->getPositionY();
 }
 
-void NtshEngn::WindowModule::setFullscreen(WindowID windowId, bool fullscreen) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	m_windows[windowId]->setFullscreen(fullscreen);
+void NtshEngn::WindowModule::setFullscreen(WindowID windowID, bool fullscreen) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	m_windows[windowID]->setFullscreen(fullscreen);
 }
 
-bool NtshEngn::WindowModule::isFullscreen(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->isFullscreen();
+bool NtshEngn::WindowModule::isFullscreen(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->isFullscreen();
 }
 
-void NtshEngn::WindowModule::setBorderless(WindowID windowId, bool borderless) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	m_windows[windowId]->setBorderless(borderless);
+void NtshEngn::WindowModule::setBorderless(WindowID windowID, bool borderless) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	m_windows[windowID]->setBorderless(borderless);
 }
 
-bool NtshEngn::WindowModule::isBorderless(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->isBorderless();
+bool NtshEngn::WindowModule::isBorderless(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->isBorderless();
 }
 
-void NtshEngn::WindowModule::setResizable(WindowID windowId, bool resizable) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	m_windows[windowId]->setResizable(resizable);
+void NtshEngn::WindowModule::setResizable(WindowID windowID, bool resizable) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	m_windows[windowID]->setResizable(resizable);
 }
 
-bool NtshEngn::WindowModule::isResizable(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->isResizable();
+bool NtshEngn::WindowModule::isResizable(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->isResizable();
 }
 
 void NtshEngn::WindowModule::pollEvents() {
 	glfwPollEvents();
 }
 
-void NtshEngn::WindowModule::setTitle(WindowID windowId, const std::string& title) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	m_windows[windowId]->setTitle(title);
+void NtshEngn::WindowModule::setTitle(WindowID windowID, const std::string& title) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	m_windows[windowID]->setTitle(title);
 }
 
-NtshEngn::InputState NtshEngn::WindowModule::getKeyState(WindowID windowId, InputKeyboardKey key) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->getKeyState(key);
+NtshEngn::InputState NtshEngn::WindowModule::getKeyState(WindowID windowID, InputKeyboardKey key) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->getKeyState(key);
 }
 
-NtshEngn::InputState NtshEngn::WindowModule::getMouseButtonState(WindowID windowId, InputMouseButton mouseButton) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->getMouseButtonState(mouseButton);
+NtshEngn::InputState NtshEngn::WindowModule::getMouseButtonState(WindowID windowID, InputMouseButton mouseButton) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->getMouseButtonState(mouseButton);
 }
 
-void NtshEngn::WindowModule::setCursorPosition(WindowID windowId, int x, int y) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	m_windows[windowId]->setCursorPosition(x, y);
+void NtshEngn::WindowModule::setCursorPosition(WindowID windowID, int x, int y) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	m_windows[windowID]->setCursorPosition(x, y);
 }
 
-int NtshEngn::WindowModule::getCursorPositionX(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->getCursorPositionX();
+int NtshEngn::WindowModule::getCursorPositionX(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->getCursorPositionX();
 }
 
-int NtshEngn::WindowModule::getCursorPositionY(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->getCursorPositionY();
+int NtshEngn::WindowModule::getCursorPositionY(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->getCursorPositionY();
 }
 
-void NtshEngn::WindowModule::setCursorVisibility(WindowID windowId, bool visible) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	m_windows[windowId]->setCursorVisibility(visible);
+void NtshEngn::WindowModule::setCursorVisibility(WindowID windowID, bool visible) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	m_windows[windowID]->setCursorVisibility(visible);
 }
 
-bool NtshEngn::WindowModule::isCursorVisible(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->isCursorVisible();
+bool NtshEngn::WindowModule::isCursorVisible(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->isCursorVisible();
 }
 
-NtshEngn::NativeWindowHandle NtshEngn::WindowModule::getNativeHandle(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->getNativeHandle();
+NtshEngn::NativeWindowHandle NtshEngn::WindowModule::getNativeHandle(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->getNativeHandle();
 }
 
-NtshEngn::NativeWindowAdditionalInformation NtshEngn::WindowModule::getNativeAdditionalInformation(WindowID windowId) {
-	NTSHENGN_ASSERT(m_windows.find(windowId) != m_windows.end());
-	return m_windows[windowId]->getNativeAdditionalInformation();
+NtshEngn::NativeWindowAdditionalInformation NtshEngn::WindowModule::getNativeAdditionalInformation(WindowID windowID) {
+	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	return m_windows[windowID]->getNativeAdditionalInformation();
 }
 
 extern "C" NTSHENGN_MODULE_API NtshEngn::WindowModuleInterface* createModule() {
--- src/ntshengn_window_module.h
+++ src/ntshengn_window_module.h
@@ -17,72 +17,72 @@ namespace NtshEngn {
 
 		// Opens a new window and returns a unique identifier
 		WindowID open(int width, int height, const std::string& title);
-		// Returns true if the window with identifier windowId is open, else, returns false
-		bool isOpen(WindowID windowId);
+		// Returns true if the window with identifier windowID is open, else, returns false
+		bool isOpen(WindowID windowID);
 		// Flags the window to be closed at the next update
-		void close(WindowID windowId);
+		void close(WindowID windowID);
 
 		// Returns the main window identifier
 		WindowID getMainWindowID();
 		// Returns the number of opened windows
 		uint64_t windowCount();
 
-		// Changes the size of the window with identifier windowId to width and height
-		void setSize(WindowID windowId, int width, int height);
-		// Returns the width of the window with identifier windowId
-		int getWidth(WindowID windowId);
-		// Returns the height of the window with identifier windowId
-		int getHeight(WindowID windowId);
+		// Changes the size of the window with identifier windowID to width and height
+		void setSize(WindowID windowID, int width, int height);
+		// Returns the width of the window with identifier windowID
+		int getWidth(WindowID windowID);
+		// Returns the height of the window with identifier windowID
+		int getHeight(WindowID windowID);
 
-		// Changes the position of the window with identifier windowId to x and y
-		void setPosition(WindowID windowId, int x, int y);
-		// Returns the horizontal position of the window with identifier windowId
-		int getPositionX(WindowID windowId);
-		// Returns the vertical position of the window with identifier windowId
-		int getPositionY(WindowID windowId);
+		// Changes the position of the window with identifier windowID to x and y
+		void setPosition(WindowID windowID, int x, int y);
+		// Returns the horizontal position of the window with identifier windowID
+		int getPositionX(WindowID windowID);
+		// Returns the vertical position of the window with identifier windowID
+		int getPositionY(WindowID windowID);
 
-		// If the fullscreen parameter is true, puts the window with identifier windowId in fullscreen, else, puts the window with identifier windowId in windowed
-		void setFullscreen(WindowID windowId, bool fullscreen);
-		// Returns true if the window with identifier windowId is in fullscreen mode, else, returns false
-		bool isFullscreen(WindowID windowId);
+		// If the fullscreen parameter is true, puts the window with identifier windowID in fullscreen, else, puts the window with identifier windowID in windowed
+		void setFullscreen(WindowID windowID, bool fullscreen);
+		// Returns true if the window with identifier windowID is in fullscreen mode, else, returns false
+		bool isFullscreen(WindowID windowID);
 
-		// If the borderless parameter is true, puts the window with identifier windowId in borderless, else, adds borders to the window with identifier windowId
-		void setBorderless(WindowID windowId, bool borderless);
-		// Returns true if the window with identifier windowId is in borderless mode, else, returns false
-		bool isBorderless(WindowID windowId);
+		// If the borderless parameter is true, puts the window with identifier windowID in borderless, else, adds borders to the window with identifier windowID
+		void setBorderless(WindowID windowID, bool borderless);
+		// Returns true if the window with identifier windowID is in borderless mode, else, returns false
+		bool isBorderless(WindowID windowID);
 
-		// If the resizable parameter is true, makes the window with identifier windowId resizable, else, makes the window with identifier windowId not resizable
-		void setResizable(WindowID windowId, bool resizable);
-		// Returns true if the window with identifier windowId is resizable, else, returns false
-		bool isResizable(WindowID windowId);
+		// If the resizable parameter is true, makes the window with identifier windowID resizable, else, makes the window with identifier windowID not resizable
+		void setResizable(WindowID windowID, bool resizable);
+		// Returns true if the window with identifier windowID is resizable, else, returns false
+		bool isResizable(WindowID windowID);
 
-		// Polls events of the window with identifier windowId
+		// Polls events of the window with identifier windowID
 		void pollEvents();
 
-		// Sets the title of the window with identifier windowId
-		void setTitle(WindowID windowId, const std::string& title);
+		// Sets the title of the window with identifier windowID
+		void setTitle(WindowID windowID, const std::string& title);
 
 		// Gets the state of the keyboard key. None is the input is neutral, Pressed the first frame it is being pressed, Held from the second frame it is pressed, Released the frame it is being released
-		InputState getKeyState(WindowID windowId, InputKeyboardKey key);
+		InputState getKeyState(WindowID windowID, InputKeyboardKey key);
 		// Gets the state of the mouse button. None is the input is neutral, Pressed the first frame it is being pressed, Held from the second frame it is pressed, Released the frame it is being released
-		InputState getMouseButtonState(WindowID windowId, InputMouseButton mouseButton);
+		InputState getMouseButtonState(WindowID windowID, InputMouseButton mouseButton);
 
 		// Sets the mouse cursor position
-		void setCursorPosition(WindowID windowId, int x, int y);
+		void setCursorPosition(WindowID windowID, int x, int y);
 		// Gets the mouse cursor horizontal position
-		int getCursorPositionX(WindowID windowId);
+		int getCursorPositionX(WindowID windowID);
 		// Gets the mouse cursor vertical position
-		int getCursorPositionY(WindowID windowId);
+		int getCursorPositionY(WindowID windowID);
 
-		// If the mouse cursor is visible in the window with identifier windowId, hides it, else, shows it
-		void setCursorVisibility(WindowID windowId, bool visible);
-		// Returns true if the mouse cursor is visible in the window with identifier windowId, else, returns false
-		bool isCursorVisible(WindowID windowId);
+		// If the mouse cursor is visible in the window with identifier windowID, hides it, else, shows it
+		void setCursorVisibility(WindowID windowID, bool visible);
+		// Returns true if the mouse cursor is visible in the window with identifier windowID, else, returns false
+		bool isCursorVisible(WindowID windowID);
 
-		// Returns the native window handle of the window with identifier windowId
-		NativeWindowHandle getNativeHandle(WindowID windowId);
-		// Returns the native window additional information of the window with identifier windowId
-		NativeWindowAdditionalInformation getNativeAdditionalInformation(WindowID windowId);
+		// Returns the native window handle of the window with identifier windowID
+		NativeWindowHandle getNativeHandle(WindowID windowID);
+		// Returns the native window additional information of the window with identifier windowID
+		NativeWindowAdditionalInformation getNativeAdditionalInformation(WindowID windowID);
 
 	private:
 		std::unordered_map<WindowID, std::unique_ptr<GLFWWindow>> m_windows;
