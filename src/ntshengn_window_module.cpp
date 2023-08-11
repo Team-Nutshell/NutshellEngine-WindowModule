d3bbdd34cbf58d6375bfd58146c622559c240ef0
ntsh-oni <teamnutshell.dev@gmail.com>
2023-08-11 14:14:23 +0200

    minor format change

--- src/ntshengn_window_module.cpp
+++ src/ntshengn_window_module.cpp
@@ -49,6 +49,7 @@ bool NtshEngn::WindowModule::isOpen(WindowID windowID) {
 
 void NtshEngn::WindowModule::close(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	m_windows[windowID]->close();
 }
 
@@ -69,61 +70,73 @@ uint64_t NtshEngn::WindowModule::windowCount() {
 
 void NtshEngn::WindowModule::setSize(WindowID windowID, int width, int height) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	m_windows[windowID]->setSize(width, height);
 }
 
 int NtshEngn::WindowModule::getWidth(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->getWidth();
 }
 
 int NtshEngn::WindowModule::getHeight(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->getHeight();
 }
 
 void NtshEngn::WindowModule::setPosition(WindowID windowID, int x, int y) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	m_windows[windowID]->setPosition(x, y);
 }
 
 int NtshEngn::WindowModule::getPositionX(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->getPositionX();
 }
 
 int NtshEngn::WindowModule::getPositionY(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->getPositionY();
 }
 
 void NtshEngn::WindowModule::setFullscreen(WindowID windowID, bool fullscreen) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	m_windows[windowID]->setFullscreen(fullscreen);
 }
 
 bool NtshEngn::WindowModule::isFullscreen(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->isFullscreen();
 }
 
 void NtshEngn::WindowModule::setBorderless(WindowID windowID, bool borderless) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	m_windows[windowID]->setBorderless(borderless);
 }
 
 bool NtshEngn::WindowModule::isBorderless(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->isBorderless();
 }
 
 void NtshEngn::WindowModule::setResizable(WindowID windowID, bool resizable) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	m_windows[windowID]->setResizable(resizable);
 }
 
 bool NtshEngn::WindowModule::isResizable(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->isResizable();
 }
 
@@ -133,56 +146,67 @@ void NtshEngn::WindowModule::pollEvents() {
 
 void NtshEngn::WindowModule::setTitle(WindowID windowID, const std::string& title) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	m_windows[windowID]->setTitle(title);
 }
 
 void NtshEngn::WindowModule::setIcon(WindowID windowID, const Image& image) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	m_windows[windowID]->setIcon(image);
 }
 
 NtshEngn::InputState NtshEngn::WindowModule::getKeyState(WindowID windowID, InputKeyboardKey key) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->getKeyState(key);
 }
 
 NtshEngn::InputState NtshEngn::WindowModule::getMouseButtonState(WindowID windowID, InputMouseButton mouseButton) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->getMouseButtonState(mouseButton);
 }
 
 void NtshEngn::WindowModule::setCursorPosition(WindowID windowID, int x, int y) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	m_windows[windowID]->setCursorPosition(x, y);
 }
 
 int NtshEngn::WindowModule::getCursorPositionX(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->getCursorPositionX();
 }
 
 int NtshEngn::WindowModule::getCursorPositionY(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->getCursorPositionY();
 }
 
 void NtshEngn::WindowModule::setCursorVisibility(WindowID windowID, bool visible) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	m_windows[windowID]->setCursorVisibility(visible);
 }
 
 bool NtshEngn::WindowModule::isCursorVisible(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->isCursorVisible();
 }
 
 float NtshEngn::WindowModule::getMouseScrollOffsetX(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->getMouseScrollOffsetX();
 }
 
 float NtshEngn::WindowModule::getMouseScrollOffsetY(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->getMouseScrollOffsetY();
 }
 
@@ -217,11 +241,13 @@ float NtshEngn::WindowModule::getMonitorDisplayScaling() {
 
 NtshEngn::NativeWindowHandle NtshEngn::WindowModule::getNativeHandle(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+
 	return m_windows[windowID]->getNativeHandle();
 }
 
 NtshEngn::NativeWindowAdditionalInformation NtshEngn::WindowModule::getNativeAdditionalInformation(WindowID windowID) {
 	NTSHENGN_ASSERT(m_windows.find(windowID) != m_windows.end());
+	
 	return m_windows[windowID]->getNativeAdditionalInformation();
 }
 
