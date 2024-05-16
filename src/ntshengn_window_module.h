effddad9d6aef7a23fe5c250ce524caa596e8e9a
ntsh-oni <teamnutshell.dev@gmail.com>
2024-05-17 01:06:38 +0200

    update includes to reflect Common changes

--- src/ntshengn_window_module.h
+++ src/ntshengn_window_module.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "../Common/module_interfaces/ntshengn_window_module_interface.h"
+#include "../Common/modules/ntshengn_window_module_interface.h"
 #include "qtwindow.h"
 #include <qapplication.h>
 #include <memory>
